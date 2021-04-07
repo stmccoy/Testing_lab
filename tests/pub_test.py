7f6bc67d813040be36111ee6c56034c2bada1390
Daniel Callus <danielcallus@gmail.com>
2021-04-07 12:28:04 +0100

    changed drink 1 and 2 objects to locals, available from pub list

--- tests/pub_test.py
+++ tests/pub_test.py
@@ -6,10 +6,10 @@ from src.drink import Drink
 class TestPub(unittest.TestCase):
     
     def setUp(self):
-        self.drink_1 = Drink("old fashioned", 5.00)
-        self.drink_2 = Drink("hophouse 13", 4.50)
+        drink_1 = Drink("old fashioned", 5.00)
+        drink_2 = Drink("hophouse 13", 4.50)
 
-        self.pub = Pub("The Prancing Pony", 100.00, [self.drink_1, self.drink_2])
+        self.pub = Pub("The Prancing Pony", 100.00, [drink_1, drink_2])
     
     def test_pub_has_name(self):
         self.assertEqual("The Prancing Pony", self.pub.name)
@@ -18,5 +18,5 @@ class TestPub(unittest.TestCase):
         self.assertEqual(list, type(self.pub.drinks_list))
 
     def test_sell_drink(self):
-        self.pub.sell_drink(self.drink_1)
+        self.pub.sell_drink(self.pub.drinks_list[0])
         self.assertEqual(105, self.pub.till)
