203812354671533d26dc6f14f7544cd5a5f06348
Daniel Callus <danielcallus@gmail.com>
2021-04-07 11:47:58 +0100

    test for name, till and drink_list

---
+++ src/pub.py
@@ -0,0 +1,5 @@
+class Pub:
+    def __init__(self, name, till, drinks_list):
+        self.name = name
+        self.till = till
+        self.drinks_list = drinks_list
--- tests/pub_test.py
+++ tests/pub_test.py
@@ -4,7 +4,10 @@ from src.pub import Pub
 class TestPub(unittest.TestCase):
     
     def setUp(self):
-        self.pub = Pub("The Prancing Pony", 100.00)
+        self.pub = Pub("The Prancing Pony", 100.00, ["old fashioned", "hophouse 13"])
     
     def test_pub_has_name(self):
         self.assertEqual("The Prancing Pony", self.pub.name)
+
+    def test_pub_has_drinks_list(self):
+        self.assertEqual(list, type(self.pub.drinks_list))
